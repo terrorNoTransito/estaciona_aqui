7e3b03ba557d0d60bea7b32efcbd1133e45c4ee0
Pedro Arruda <pedrooliveira2702@gmail.com>
2023-06-19 22:37:34 -0300

    <codigo>

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const path =require('path')
 
 app.set('view engine','ejs');
-
+app.set('views',path.join(__dirname,'views'))
+app.use(express.static(path.join(__dirname,"public")))
 //O problema do Css
 //Colocar o codigo  da tela no node js
 //Mostrar 1 tela para 2 diferentes usuarios com alteração remota
@@ -11,7 +12,7 @@ app.set('view engine','ejs');
 //Organizar o node js nas routers-Pode ser resolvido no próxima acompanhamento
 app.get('/',(req,res) =>{
 
-    res.render('index')
+    res.render('selecao_vagas')
 
 } )
 
